3f77581b0f8c87bb156fef5ff2f7e80d311ca873
betultultay <betultultaybusiness@gmail.com>
2023-07-31 12:50:08 +0300

    Initial commit

---
+++ src/main/java/com/example/buildingmanagement/BuildingmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buildingmanagement/BuildingmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
